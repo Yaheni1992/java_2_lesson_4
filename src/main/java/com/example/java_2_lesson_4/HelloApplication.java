caf2725ed2458d8fd9bf73e631efc3b5c92a7ba2
Yaheni1992 <neo_jeka@mail.ru>
2022-02-14 18:22:03 +0300

    дз4,не успел доделать.8 версия не хотела рабоать,пришлось с 17 работать,и код перебирать заново.

--- src/main/java/com/example/java_2_lesson_4/HelloApplication.java
+++ src/main/java/com/example/java_2_lesson_4/HelloApplication.java
@@ -1,11 +1,20 @@
 package com.example.java_2_lesson_4;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloApplication extends Application {
-
-
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Window");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
-
